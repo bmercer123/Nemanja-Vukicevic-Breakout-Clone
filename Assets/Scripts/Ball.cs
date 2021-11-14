89cd68a5bf84b2039323134b381630494ce354ce
bmercer123 <59985570+bmercer123@users.noreply.github.com>
2021-11-15 00:26:13 +1300

    Paddle and ball, basic movement and collision

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }  
+        if(Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Bottom")) // if the collision is with the bottom collider
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed; //leaving as public for easier control through inspector
+    public float rSE;
+    public float lSE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Paddle Movement
+        //Matching the float with the Input Manager
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //defining the movement od the paddle by horizontal input
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+        //controlling paddle does not go off screen
+        if(transform.position.x < lSE) //checking left screen edge
+        {
+            transform.position = new Vector2(lSE, transform.position.y);
+        }
+        if (transform.position.x > rSE) //checking right screen edge
+        {
+            transform.position = new Vector2(rSE, transform.position.y);
+        }
+    }
+}
